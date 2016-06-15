2baff7e2fb7a57b160262cf9a143ad3f1e4db6b2
Hookist <spartanstas@gmail.com>
2016-06-15 12:02:23 +0300

    Add files via upload

--- User.php
+++ User.php
@@ -1,9 +1,16 @@
 <?php
+include("DBContext.php");
     class User
     {
         public $username;
         private $isLogged = false;
-        
+        private $connector;
+
+        public function User()
+        {
+            $this->connector = new Connector();
+        }
+
         public function login()
         {
             
@@ -11,11 +18,145 @@
         
         public function logout()
         {
+
         }
         
         public function isAdmin()
         {
             return $this->isLogged;
         }
+
+        private function CheckLoginForMistakes($username)
+        {
+            echo "Login count = " . strlen($username);
+            if(strlen(isset($username)? $username:null) < 4)
+            {
+                echo "Username must has more then 4 symbols";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private function CheckPasswordForMistakes($password, $repassword)
+        {
+            if(strcmp(isset($password)?$password:null,
+                    isset($repassword)?$repassword:null) == 0)
+            {
+                echo "Passwords are the same";
+                if(strlen(isset($password)?$password:null) > 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    echo "Password must has more then 4 symbols";
+                    return false;
+                }
+            }
+            else
+            {
+                echo "Passwords are not the same";
+                return false;
+            }
+
+        }
+
+        private function ValidateUser($login, $password, $repassword)
+        {
+
+            $res = $this->connector->doQuery("SELECT `UserName` FROM `User` WHERE `UserName`= '$login';");
+
+            $count = $res->num_rows;
+            if($count > 0)
+            {
+                echo "We already have that username, please write another";
+            }
+            else
+            {
+                $isRightLogin = $this->CheckLoginForMistakes($login);
+                $isRightPassword = $this->CheckPasswordForMistakes($password, $repassword);
+                if($isRightLogin == true && $isRightPassword == true)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+
+        public function AddNewUser($username, $password, $repassword)
+        {
+            if($this->ValidateUser($username, $password, $repassword)) {
+
+
+                $res = $this->connector->doQuery("INSERT INTO `user` (`UserName`, `Password`)
+            VALUES ('$username', '$password');");
+                if ($res == true) {
+                    echo "Dobavilosia " . "<br>";
+                } else
+                    echo "Ne dobavilosia" . "<br>";
+            }
+        }
+
+
+        public function CheckUser($login, $password)
+        {
+            $login = isset($login)?$login:null;
+            $password = isset($password)?$password:null;
+
+            echo " login : " . $login . "<br>";
+            echo " password : " . $password . "<br>";
+
+            $res = $this->connector->doQuery("SELECT * FROM `User` WHERE `UserName` = '$login' AND `Password` = '$password';");
+
+            echo "Numbers of column : " . $res->num_rows;
+            if($res->num_rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                echo "Wrong username or password";
+                return false;
+            }
+        }
+
+
+
+        function CheckString($someString)
+        {
+            if(strripos($someString, " ") != false)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
     }
+
+    class A
+    {
+        public static function Gde()
+        {
+            echo __CLASS__;
+        }
+        public static function Uznat()
+        {
+            static::Gde();
+        }
+
+    }
+
+    class B extends A
+    {
+        public static function Gde()
+        {
+            echo __CLASS__;
+        }
+    }
+
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,16 @@
 <?php
-    include("DBContext.php");
-
-    $connector = new Connector();
-
-    $connector->RegisterNewUser($_POST['username'], $_POST['password'], $_POST['repassword']);
- 
+    include("User.php");
+    $user = new User();
+    session_start();
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        if ($user->CheckUser($_POST['username'], $_POST['password']))
+        {
+            echo "PRAViLNO";
+            $_SESSION['username'] = $_POST['username'];
+            echo '<script type="text/javascript"> window.location = "http://localhost/PHP_Les9/admin/index.php" </script>';
+        }
+    }
 ?>
 
 <html>
@@ -20,41 +26,37 @@
     <link href="mystylesheet.css" rel="stylesheet">
 
 </head>
+
+
 <body>
+
 <div class = "container">
     <div class="wrapper">
-        <form action="" method="post" class="form-signin">
-            <h3 class="form-signin-heading">Please register</h3>
-            <hr class="colorgraph"><br>
+
+        <form action="" method="post" name="Login_Form" class="form-signin">
+            <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
+            <hr class="colorgraph"/><br>
             <div class="input-group">
 
                 <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="username" required="" autofocus="">
             </div>
 
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Login" aria-describedby="basic-addon2"
-                       name="mail">
 
+
+            <div class="input-group">
+                <input type="password" class="form-control" placeholder="Password" aria-label="Amount (to the nearest dollar)" name="password" required="">
 
             </div>
 
-            <div class="input-group">
-
-                <input type="text" class="form-control" placeholder="password" aria-label="Amount (to the nearest dollar)" name="password">
-
-            </div>
-
-            <div class="input-group">
-
-                <input type="text" class="form-control" placeholder="password again" aria-label="Amount (to the nearest dollar)" name="repassword">
-
-            </div>
-            <button type="submit"  class="btn btn-lg btn-primary btn-block">
-                Registr
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="Submit">
+                AUTORIZE
 
             </button>
+
         </form>
     </div>
 </div>
+
+
 </body>
 </html>
